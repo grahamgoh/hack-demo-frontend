9321a568f4c858b9146f3e251c2c11b12b6b9eb4
Graham Goh <ggoh@seek.com.au>
2018-11-25 13:22:11 +1100

    update config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  // GENERATOR_API_URL: 'https://floating-scrubland-34728.herokuapp.com'
-  GENERATOR_API_URL: 'http://localhost:3000'
+  GENERATOR_API_URL: 'https://floating-scrubland-34728.herokuapp.com'
+  // GENERATOR_API_URL: 'http://localhost:3000'
 
 };
